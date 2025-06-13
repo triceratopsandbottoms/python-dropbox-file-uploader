4d01fd32972757fc1dd7fd0041448c5668b0c071
Aya <tricera@duck.com>
2025-06-13 14:51:53 +0200

    added functionality: download_files, renamed DropboxUploader -> DropboxHandler externalized input for file paths

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os
 import dropbox
 import requests
 import configparser
+from dotenv import dotenv_values
 
-class DropboxUploader:
+# load secrets from file
+secrets = dotenv_values("../.env")
+
+credentials_dir = secrets['DROPBOXUPLOADER_CREDENTIALS_DIR']
+
+class DropboxHandler:
     def __init__(self):
         # Configuration parameters
         self.source_directory = ""
         self.local_file_path = 'file.txt'
-        self.credentials_directory = "credentials.ini"
-        self.dropbox_directory = "/file_backups"
-        
+        self.credentials_directory = credentials_dir
+        self.dropbox_directory = ""
         # Initialize Dropbox API with access token
         self.access_token = self.read_credentials_value("Authentication", "access_token")
         self.dbx = dropbox.Dropbox(self.access_token)
@@ -61,9 +66,9 @@ class DropboxUploader:
         # Check if the existing token is still valid
         try: 
             self.dbx.files_list_folder('')
-            print("Token is valid")
+            #print("Token is valid")
         except dropbox.exceptions.AuthError:
-            print("Token is expired, generating new access token ...")
+            #print("Token is expired, generating new access token ...")
             app_key = self.read_credentials_value("Authentication", "app_key")
             app_secret = self.read_credentials_value("Authentication", "app_secret")
             refresh_token = self.read_credentials_value("Authentication", "refresh_token")
@@ -76,25 +81,36 @@ class DropboxUploader:
         except dropbox.exceptions.DropboxException as e:
             print("check_token_validity function: An error occurred while checking the token:", e)
 
-    def upload_files(self):
+    def upload_files(self, file, saveAs):
         # Upload a file to Dropbox
         try:
             self.check_token_validity()
-            file_name = os.path.basename(self.local_file_path)
+            #file_name = os.path.basename(self.local_file_path)
 
-            with open(self.local_file_path, "rb") as file:
-                self.dbx.files_upload(file.read(), f"{self.dropbox_directory}/{file_name}", mode=dropbox.files.WriteMode("overwrite"))
-                print(f"File '{file_name}' uploaded successfully to Dropbox.")
+            #with open(self.local_file_path, "rb") as file:
+            self.dbx.files_upload(file, f"{self.dropbox_directory}/{saveAs}", mode=dropbox.files.WriteMode("overwrite"))
+            #print(f"File '{saveAs}' uploaded successfully to Dropbox.")
 
         except Exception as e:
-            print("upload_files function: An error occurred:", e)
+            print(f"upload_files function: An error occurred while saving {saveAs}:", e)
+            
+    def download_files(self, db_file_path, local_file_path):
+        # Download a file from Dropbox
+        try:
+            self.check_token_validity()
 
-def main():
-    # Main program flow
-    print("Start uploading files ...")
-    dropbox_uploader = DropboxUploader()
-    dropbox_uploader.upload_files()
-    print("End uploading files")
+            self.dbx.files_download_to_file(local_file_path, f"{self.dropbox_directory}/{db_file_path}")
+            print(f"File '{db_file_path}' downloaded successfully to '{local_file_path}'.")
 
-if __name__ == "__main__":
-    main()
+        except Exception as e:
+            print(f"upload_files function: An error occurred while saving {local_file_path}:", e)
+
+# def main():
+    # # Main program flow
+    # print("Start uploading files ...")
+    # dropbox_uploader = DropboxUploader()
+    # dropbox_uploader.upload_files('muss das wirklich sein', 'text.txt')
+    # print("End uploading files")
+
+# if __name__ == "__main__":
+    # main()
